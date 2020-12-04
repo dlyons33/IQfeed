259f17aa37ca1c9ab745be948372c2e6323f8026
Dennis Lyons <dlyons@gkllaw.com>
2020-12-04 11:03:51 -0500

    Connection definitions & test script

--- connection.py
+++ connection.py
@@ -1,4 +1,3 @@
-import numpy as np
 import socket
 import threading
 
@@ -7,8 +6,6 @@ Next step:
 read_messages & process_messages
 '''
 
-SYMBOLS = ['SPY']
-
 class BarsConnection():
 
     def __init__(self,host,port):
@@ -16,43 +13,85 @@ class BarsConnection():
         self._host = host
         self._port = port
 
-        self._listeners = []
-
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self._stop = threading.Event()
         self._reader_thread = thrading.Thread(group=None,target=self,name=self._name,
                                                 args=(),kwargs={},daemon=None)
 
-        self._buffd_data = ''
+        self._buffered_data = ''
 
     def __call__(self):
         while not self._stop.is_set():
-            if self.read_messages(): # Test out None/True/False returns
+            if self.read_socket():
                 self.process_messages()
 
     def connect(self):
         '''
         Need 'S,CONNECT' or 'S,Set Client Name,' messages to gateway?
         '''
-        self._sock.connect((self._host,self._port))
-        self.start_reader()
+        try:
+            self._sock.connect((self._host,self._port))
+            self.start_reader()
+            print('Socket connected & reader thread started!')
+        except Exception as e:
+            print('\n===============================================\n')
+            print('ERROR connecting to socket or starting thread!')
+            print(e)
+            print('\n===============================================\n')
+
+    def disconnect(self):
+        self.send_cmd('S,UNWATCH ALL') # terminate subscriptions?
+        self.stop_reader()
+        if self._sock:
+            self._sock.shutdown(socket.SHUT_RDWR)
+            self._sock.close()
+            self._sock = None
 
     def start_reader(self):
         self._stop.clear()
         if not self._reader_thread.is_alive():
             self._reader_thread.start()
 
-    def read_messages(self):
-        data_received = self._sock.recv(1024).decode('latin-1')
+    def stop_reader(self):
+        self._stop.set()
+        if self._reader_thread.is_alive():
+            self._reader_thread.join(30)
+        if self._reader_thread.is_alive():
+            print('ERROR! Reader thread still ALIVE!')
+
+    def read_socket(self):
+        data_received = self._sock.recv(1024).decode()
+        if data_received:
+            print('\nNew data received from socket:')
+            print(data_received,'\n')
+            self._buffered_data += data_received
+            return True
+        else:
+            return False
+
+    def next_message(self):
+        next_delim = self._buffered_data.find('\n')
+        if next_delim != -1:
+            message = self._buffered_data[:next_delim].strip()
+            print('\nNew full message buffered:')
+            print(message,'\n')
+            self._buffered_data = self._buffered_data[(next_delim + 1):]
+            return message
+        else:
+            return ''
 
     def process_messages(self):
-        pass
+        message = self.next_message()
+        while message != '':
+            fields = message.split(',')
+            print('\n===============================================\n')
+            for f in fields:
+                print(f)
+            print('\n===============================================\n')
+            message = self.next_message()
 
-    def add_listener(self,listener):
-        if listener not in self._listeners:
-            self._listeners.append(listener)
-
-    def remove_listener(self,listener):
-        if listener in self._listeners:
-            self._listeners.remove(listener)
+    def send_cmd(cmd):
+        self._sock.sendall(cmd.encode(encoding='latin-1'))
+        print('\nSent string:')
+        print(cmd,'\n')
---
+++ test_connect.py
@@ -0,0 +1,45 @@
+from time import sleep
+import connection
+
+if __name__ == "__main__":
+
+    host = "127.0.0.1"  # Localhost
+    port = # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    print('Initilizing connection...')
+    conn = connection.BarsConnection(host,port)
+    conn.connect()
+
+    print('Requesting subscription to SPY...')
+
+    '''
+    BW,[Symbol],[Interval],[BeginDate BeginTime],[MaxDaysOfDatapoints],
+    [MaxDatapoints],[BeginFilterTime],[EndFilterTime],[RequestID],
+    [Interval Type],[Reserved],[UpdateInterval]
+    '''
+
+    symbol = 'SPY'
+    interval_len = 5
+    bgn_bar_str
+    lookback_days_str
+    lookback_bars_str
+    bf_str
+    ef_str
+    request_id = 'test'
+    interval_type = 's'
+    update_str
+
+    cmd =   "BW,%s,%s,%s,%s,%s,%s,%s,%s,%s,'',%s\r\n" % (
+            symbol, interval_len, bgn_bar_str, lookback_days_str,
+            lookback_bars_str,
+            bf_str, ef_str, request_id, interval_type, update_str)
+
+    conn.send_command(cmd)
+
+    print('Going to sleep...')
+    sleep(20)
+
+    print('CLOSING CONNECTION...')
+    conn.disconnect()
+
+    print('All done!')
